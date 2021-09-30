de57cc1a71db6b443a373f834a5029cd0486a4f6
Davi Scholl <davis@bandavide.com.br>
2021-09-30 00:37:51 -0300

    implemented forms

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,28 @@
 class Task < ApplicationRecord
   validates_presence_of :description
   validates :done, inclusion: [true, false]
+
+  def symbol
+    case status
+      when 'pending' then '>>'
+      when 'done'    then 'v'
+      when 'expired' then 'x'
+    end
+  end
+
+  def css_color
+    case status
+      when 'pending' then 'primary'
+      when 'done'    then 'success'
+      when 'expired' then 'danger'
+    end
+  end
+
+  private
+
+  def status
+    return 'done' if done?
+
+    due_date.past? ? 'expired' : 'pending'
+  end
 end
